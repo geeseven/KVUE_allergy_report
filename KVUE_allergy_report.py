695b098c5bea50f27dd16e73dd1b3a09243ecada
geeseven <2334728+geeseven@users.noreply.github.com>
2020-11-27 13:23:30 -0600

    add the script and needed bits

---
+++ KVUE_allergy_report.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from datetime import date
+
+from bs4 import BeautifulSoup
+from rich.console import Console
+from rich.table import Table
+from selenium import webdriver
+
+# iframe from https://www.kvue.com/allergy
+url = "https://www.keepandshare.com/calendar/show_month.php?i=1940971"
+
+logLocation = "/dev/null"
+# For debugging
+# logLocation = "./geckodriver.log"
+opts = webdriver.FirefoxOptions()
+opts.headless = True
+brower = webdriver.Firefox(options=opts, service_log_path=logLocation)
+
+brower.get(url)
+soup = BeautifulSoup(brower.page_source, "html.parser")
+brower.close()
+
+# the background color for today is a different color than all other days
+allergyReport = soup.find("span", {"style": "background-color: #ffffff;"})
+
+# KVUE has allergy reports almost everyday
+# maybe figure out how to get the most recent report instead of just today
+if not allergyReport:
+    print(
+        "KVUE does not have an allergy report today."
+        "For recent reports, check {}".format(url)
+    )
+    exit()
+
+# sometime the allery report ends with a ".", lets remove it & spilt the report
+if allergyReport.text[-1] == ".":
+    allergies = allergyReport.text[0:-1].split(", ")
+else:
+    allergies = allergyReport.text.split(", ")
+
+table = Table(title="KVUE allergy report for {}".format(date.today()))
+table.add_column("allergen")
+table.add_column("severity")
+
+for item in sorted(allergies):
+    # change formatting from 'Trees 13 gr/m3 Low' to ['Trees', '13 gr/m3 Low']
+    i = item.split(" ", 1)
+    table.add_row(i[0], i[1])
+
+Console().print(table)
